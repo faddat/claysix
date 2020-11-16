355a09eda76853616b683407bb18b364c66b426f
Developer Experience team at Tendermint <hello@tendermint.com>
2020-11-17 01:14:47 +0700

    Initialized with Starport

---
+++ x/claysix/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/faddat/claysix/x/claysix/types"
+)
+
+// GetParams returns the total set of claysix parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the claysix parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
